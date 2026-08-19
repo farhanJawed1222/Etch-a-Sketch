de676706dc9d0ba2ecabfd00c4b33586eb71c954
Farhan Jawed <farhanjawed123098@gmail.com>
2026-08-19 12:29:42 +0530

    add random color on hover

--- script.js
+++ script.js
@@ -2,13 +2,20 @@
 const grid_container = document.querySelector(".grid__container");
 const start_btn = document.querySelector(".start__btn");
 
+//-------------------------------------function--------------------------------------------
+function getRandomColor(e) {
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    e.target.style.background = `rgb(${red}, ${green}, ${blue})`
+}
 
 //--------------------------------Event Listener-----------------------------------------
 start_btn.addEventListener("click", (e) => {
     let totalGrid = prompt("Total grid per side? (max 100)");
 
     // check for number greater than 100
-    if(totalGrid < 1 || totalGrid > 100){
+    if (totalGrid < 1 || totalGrid > 100) {
         alert("Enter number between 1 and 100");
         return;
     };
@@ -27,7 +34,11 @@ start_btn.addEventListener("click", (e) => {
             grid.style.width = width + 'px';
             grid.style.height = height + 'px';
             grid_container.appendChild(grid);
+
+            // add random color on each grid on hovering
+            grid.addEventListener("mouseover", getRandomColor);
+
         }
     }
-    
+
 });
