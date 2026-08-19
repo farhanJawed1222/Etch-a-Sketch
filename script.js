6f9903632ef50dbf919fadeb714313f29a0d14f3
Farhan Jawed <farhanjawed123098@gmail.com>
2026-08-19 11:51:06 +0530

    generate grid dynamically based on user input

---
+++ script.js
@@ -0,0 +1,31 @@
+// ---------------------------------------Reference----------------------------------------
+const grid_container = document.querySelector(".grid__container");
+const start_btn = document.querySelector(".start__btn");
+
+
+//--------------------------------Event Listener-----------------------------------------
+start_btn.addEventListener("click", (e) => {
+    let totalGrid = prompt("Total grid per side? (max 100)");
+
+    // check for number greater than 100
+    if(totalGrid < 1 || totalGrid > 100){
+        alert("Enter number between 1 and 100");
+        return;
+    };
+
+    // dimension of each grid
+    let width = 960 / totalGrid;
+    let height = 960 / totalGrid;
+
+    //create a sketchpad of different grid size
+    for (let i = 0; i < totalGrid; i++) {
+        for (let j = 0; j < totalGrid; j++) {
+            let grid = document.createElement("div");
+            grid.classList.add("grid");
+            grid.style.width = width + 'px';
+            grid.style.height = height + 'px';
+            grid_container.appendChild(grid);
+        }
+    }
+    
+});
